de69a671a3a4069b8a578b38997eb34d9e4f865e
Jackson Strauss <58577918+Vaultboy58@users.noreply.github.com>
2020-04-08 23:51:53 -0500

    Add files via upload

---
+++ team.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS team_db
+
+CREATE DATABASE team_db
+
+
+
+create TABLE teammates (
+  
+teammateID INT,
+    Last_Name varchar(30),
+    First_Name varchar(30),   
+    role_ID INT,
+    manager_ID INT,
+); 
+
+
+
+
+
+ 
+
+
+
+
